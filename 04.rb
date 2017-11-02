7faf78043deecb8ee16f069fcfbaef88072bd635
TygranGorelov <tigran.gorelov@faceit-team.com>
2017-11-02 16:48:38 +0200

    deleted comments

--- 04.rb
+++ 04.rb
@@ -1,32 +1,3 @@
-# array = [ 0, 1, 0, 3, 4, 8, 0]
-#
-# p "Четные "
-# i=0
-# while i <= array.length
-#   p array[i]
-#   i+=2
-# end
-#
-# p "Нечетные "
-#
-# i=1
-# while i <= array.length
-#   p array[i]
-#   i+=2
-# end
-
-
-
-# [ 0, 1, 0, 3, 4, 8, 0].each_with_index do |value, index|
-#   if index.even?
-#     p 'Четные ' + "#{index}: #{value}".to_s
-#   else
-#     p 'Нечетные ' + "#{index}: #{value}".to_s
-#   end
-# end
-
-
-
 a=[]
 b=[]
 [ 0, 1, 0, 3, 4, 8, 0].each_with_index do |value, index|
@@ -36,11 +7,7 @@ b=[]
     b.push "#{value}"
   end
 end
-# p 'Четные ' + a.to_s
-# p 'Нечетные ' + b.to_s
-
 p 'Четные'
 p a
 p 'Нечетные'
 p b
-
