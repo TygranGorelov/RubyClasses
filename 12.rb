46e3aaff1b9e9fb479336e5dc3486d76132e596a
TygranGorelov <tigran.gorelov@faceit-team.com>
2017-11-03 18:36:12 +0200

    Accepted 12 13 14 15

--- 12.rb
+++ 12.rb
@@ -1,3 +1,4 @@
+# accepted
 array = [37.5, 84.7, 62.6, 91.2, 11.4, 65.5, 57.6, 28.4, 19.1, 49.5]
 R = 29.4
 p 'Исходный массив ' + array.to_s
@@ -6,8 +7,8 @@ p 'Заданное число ' + R.to_s
 r_mod = R.abs
 rezult = 0
 array.each do |x|
-  if(R-x).abs < r_mod
-    r_mod = ( R - x).abs
+  if (R - x).abs < r_mod
+    r_mod = (R - x).abs
     rezult = x
   end
 end
--- 14.rb
+++ 14.rb
@@ -1,6 +1,7 @@
+# accepted
 N = 999125444444444
 p 'Исходное число ' + N.to_s
 p n_str = N.to_s
-p array = n_str.split(//)
+p array = n_str.split('')
 p 'Максимальное число ' + array.max.to_s
 p 'Количество вхождений ' + array.count { |x| x == array.max }.to_s
