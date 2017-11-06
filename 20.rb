fd0e00c34408353e306c5031d405500317879d61
TygranGorelov <tigran.gorelov@faceit-team.com>
2017-11-06 17:11:46 +0200

    Done 20

---
+++ 20.rb
@@ -0,0 +1,24 @@
+INIT_HASH = {
+  a: 12,
+  b: 74.9,
+  c: [23, 44, 66, -8, 124],
+  d: 'Some String',
+  z: { ca: 'Inherit hash', cb: 112 }
+}.freeze
+p "Исходный ХЭШ --> #{INIT_HASH}"
+
+hash = INIT_HASH.dup
+hash.delete_if { |_, v| v.is_a?(String) || v.is_a?(Numeric) }
+p "Удалены строки и числа --> #{hash}"
+
+
+hash2 = INIT_HASH.dup
+keys = []
+values = []
+hash2.each do |key, value|
+  keys << key
+  values << value
+end
+keys_r = keys.reverse
+rez = keys_r.zip(values)
+p "Реверс ключей не меняя значения --> #{rez.to_h}"
