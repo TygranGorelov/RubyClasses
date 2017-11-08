cc58497461416786f146533f39dc5d1a9f02773c
TygranGorelov <tigran.gorelov@faceit-team.com>
2017-11-08 14:14:54 +0200

    Done 02

--- 02.rb
+++ 02.rb
@@ -1,7 +1,20 @@
-i=0
-loop do
-  i+=1
-  next if i==5
-  print "#{i} "
-  break if i==10
+puts 'Hello. Please enter a radius: '
+
+r = gets.to_i
+y = r
+r_in = r - 0.4
+r_out = r + 0.4
+
+until y < -r do
+  x = -r
+  until x >= r_out do
+    if x * x + y * y >= r_in * r_in && x * x + y * y <= r_out * r_out
+      print '*'
+    else
+      print '-'
+    end
+    x += 0.5
+  end
+  puts "\n"
+  y -= 1
 end
