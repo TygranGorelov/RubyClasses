da48736ed2e2c073811c62691e0e6ba5113abede
TygranGorelov <tigran.gorelov@faceit-team.com>
2017-11-08 11:04:08 +0200

    Done 22

--- 22.rb
+++ 22.rb
@@ -1,50 +1,30 @@
-# Написать рекурсивный метод для поиска значения заданого ключа пользователем в хеше:
-
 INIT_HASH = {
-    key1: 'Xaxaxa',
+    key1: 'FIRST',
     key2: 'SECOND',
     key3: {
-        key4: 'str',
-        key5: 'str2',
+        key4: 'FOURTH',
+        key5: 'FIFTH',
         key6: {
             key7: {
-                key8: 1,
-                key9: [2]}
+                key8: 'EIGHTH',
+                key9: ['NINETH']}
         }
     }
 }
-
-#p INIT_HASH[:key1] #- ОБРАЩЕНИЕ К ЭЛЕМНТУ ХЕША!!!!
-
-#p INIT_HASH.keys
-
-
-
-# p hash.class - определить тип переменной
 
 puts 'Введите ключ: '
 
 u_key = gets.strip
 user_key = u_key.to_sym
 
-#ПЕРЕДЕЛАТЬ
-def search(key)
-  if INIT_HASH.has_key?(key) #Если в нашем хэше INIT_HASH есть ключ key, то выводим этот ключ. Если нету, то идем в else
-    return INIT_HASH[key]  #работает
+main_hash = INIT_HASH
 
-  else
-    search(INIT_HASH[key]) #надо вызвать ф-цию search на элементы key1, key2, key3
+def search(m_h, u_k)
+  return m_h[u_k] if m_h.has_key? u_k
+  m_h.each do |_, value|
+    return search(value, u_k) if value.is_a? Hash
   end
 end
 
-p search(user_key)
+p search(main_hash, user_key)
 
-# def search(key)
-#   if INIT_HASH.has_key?(key) #Если в нашем хэше INIT_HASH есть ключ key, то выводим этот ключ. Если нету, то идем в else
-#     return INIT_HASH[key]  #работает
-#
-#   else
-#     search(INIT_HASH[key]) #надо вызвать ф-цию search на элементы key1, key2, key3
-#   end
-# end
-# p search(user_key)
